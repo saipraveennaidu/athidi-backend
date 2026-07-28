bfb7711ad1faf3b9b718084e33c265fedac237cd
Sai Praveen Naidu <saipraveennaidupudi@gmail.com>
2026-07-28 18:50:34 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/athidi/AthidiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.athidi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AthidiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AthidiBackendApplication.class, args);
+	}
+
+}
